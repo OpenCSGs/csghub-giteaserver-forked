862d3b95c7e33cba11a855c3e59b4f77a270b411
vincent <pulltheflower@163.com>
2024-02-19 14:06:46 +0800

    Fix update lfs file content size does not match bug (#2)
    * Fix update lfs file content size does not match bug
    
    * Add err check

--- services/repository/files/update.go
+++ services/repository/files/update.go
@@ -447,6 +447,10 @@ func CreateOrUpdateFile(ctx context.Context, t *TemporaryUploadRepository, file 
 			return err
 		}
 		if !exist {
+			_, err := file.ContentReader.(io.Seeker).Seek(0, io.SeekStart)
+			if err != nil {
+				return err
+			}
 			if err := contentStore.Put(lfsMetaObject.Pointer, file.ContentReader); err != nil {
 				if _, err2 := git_model.RemoveLFSMetaObjectByOid(ctx, repoID, lfsMetaObject.Oid); err2 != nil {
 					return fmt.Errorf("unable to remove failed inserted LFS object %s: %v (Prev Error: %w)", lfsMetaObject.Oid, err2, err)
